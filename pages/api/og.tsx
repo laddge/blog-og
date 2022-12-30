ad84d4f7e340f2875c6e55e45012ef799dcde079
Laddge <dev.laddge@gmail.com>
2022-12-30 11:20:38 +0000

    位置調整

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -130,59 +130,70 @@ export default async function og(req: NextRequest) {
             style={{
               display: 'flex',
               width: '100%',
-              minHeight: '33%',
-              maxHeight: '40%',
-              marginTop: '16px',
-              padding: '24px 48px',
-              fontSize: '56px',
+              height: '454px',
+              flexWrap: 'wrap',
               textAlign: 'center',
-              alignItems: 'flex-end',
+              alignContent: 'center',
               justifyContent: 'center',
             }}
           >
-            {title}
-          </div>
-          <div
-            style={{
-              display: 'flex',
-              width: '100%',
-              fontSize: '40px',
-              color: '#767676',
-              textAlign: 'center',
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}
-          >
-            {date}
-          </div>
-          <div
-            style={{
-              display: 'flex',
-              width: '100%',
-              marginTop: '24px',
-              fontSize: '30px',
-              color: '#767676',
-              textAlign: 'center',
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" height="30" style={{ marginRight: '16px' }} fill="#767676"><path d="M447.1 96h-172.1L226.7 50.75C214.7 38.74 198.5 32 181.5 32H63.1c-35.35 0-64 28.66-64 64v320c0 35.34 28.65 64 64 64h384c35.35 0 64-28.66 64-64V160C511.1 124.7 483.3 96 447.1 96zM463.1 416c0 8.824-7.178 16-16 16h-384c-8.822 0-16-7.176-16-16V96c0-8.824 7.178-16 16-16h117.5c4.273 0 8.293 1.664 11.31 4.688L255.1 144h192c8.822 0 16 7.176 16 16V416z"/></svg>
-            {category}
-          </div>
-          <div
-            style={{
-              display: 'flex',
-              width: '100%',
-              marginTop: '20px',
-              fontSize: '24px',
-              color: '#1a7999',
-              textAlign: 'center',
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}
-          >
-            {tags}
+            <div
+              style={{
+                display: 'flex',
+                width: '100%',
+                padding: '0 48px',
+                fontSize: '56px',
+                lineHeight: '80px',
+                textAlign: 'center',
+                alignItems: 'flex-end',
+                justifyContent: 'center',
+              }}
+            >
+              {title}
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                width: '100%',
+                marginTop: '24px',
+                fontSize: '40px',
+                color: '#767676',
+                textAlign: 'center',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              {date}
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                width: '100%',
+                marginTop: '24px',
+                fontSize: '30px',
+                color: '#767676',
+                textAlign: 'center',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" height="30" style={{ marginRight: '16px' }} fill="#767676"><path d="M447.1 96h-172.1L226.7 50.75C214.7 38.74 198.5 32 181.5 32H63.1c-35.35 0-64 28.66-64 64v320c0 35.34 28.65 64 64 64h384c35.35 0 64-28.66 64-64V160C511.1 124.7 483.3 96 447.1 96zM463.1 416c0 8.824-7.178 16-16 16h-384c-8.822 0-16-7.176-16-16V96c0-8.824 7.178-16 16-16h117.5c4.273 0 8.293 1.664 11.31 4.688L255.1 144h192c8.822 0 16 7.176 16 16V416z"/></svg>
+              {category}
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                width: '100%',
+                marginTop: '20px',
+                fontSize: '24px',
+                color: '#1a7999',
+                textAlign: 'center',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              {tags}
+            </div>
           </div>
         </div>
       </div>
